6405943638370ce415e9be3d8d2950d743904424
Aswin301 <aswin0034@gmail.com>
2023-04-01 23:49:29 +0530

    Succes again

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Login from './components/Login';
 import Dashboard from './components/Dashboard';
 function App() {
   return (
-    <BrowserRouter  basename = "users-app">
+    <BrowserRouter>
     <Routes>
       <Route path="/" element={<Dashboard/>}/>
       <Route path="/Login" element={<Login/>}/>
